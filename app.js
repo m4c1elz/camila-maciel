3745ea14ee90e9a2172569b3285b751d9029558e
unknown <milaelipe.maciel@gmail.com>
2024-01-13 12:26:27 -0300

    salvando tudo

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from "express"
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express()
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const port = 3000
+
+app.set("view engine", "ejs")
+
+app.use(express.static(__dirname + "/public"))
+
+app.get('/', (req, res) => {
+    res.redirect("/home")
+})
+
+app.get('/home', (req, res) => {
+    res.render("index")
+}) 
+
+app.get("/sobremim", (req, res) => {
+    res.render("about")
+})
+
+app.get("/solucoes", (req, res) => {
+    res.render("solutions")
+})
+
+app.listen(port, () => {
+    console.log("Servidor iniciado na porta " + port)
+    console.log("Acesse em: localhost:" + port)
+})
