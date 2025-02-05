0979524c1410248b98c7b14bf396084109757610
m4c1elz <milaelipe.maciel@gmail.com>
2025-02-05 18:15:09 -0300

    feat: adapt project to vercel environment

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
-import express from "express"
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import express from 'express'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url))
 const port = 3000
 
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs')
 
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-    res.redirect("/home")
+    res.redirect('/home')
 })
 
 app.get('/home', (req, res) => {
-    res.render("index")
-}) 
-
-app.get("/sobremim", (req, res) => {
-    res.render("about")
+    res.render('index')
 })
 
-app.get("/solucoes", (req, res) => {
-    res.render("solutions")
+app.get('/sobremim', (req, res) => {
+    res.render('about')
 })
 
-app.get("*", (req, res) => {
-    res.status(404).render("error")
+app.get('/solucoes', (req, res) => {
+    res.render('solutions')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).render('error')
 })
 
 app.listen(port, () => {
-    console.log("Servidor iniciado na porta " + port)
-    console.log("Acesse em: localhost:" + port)
+    console.log('Servidor iniciado na porta ' + port)
+    console.log('Acesse em: localhost:' + port)
 })
+
+export { app }
